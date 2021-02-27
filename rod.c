5ececd35570dea29da23c996a75bd4b74bcb77df
Connor Yu <connordyu@gmail.com>
2021-02-26 23:00:41 -0500

    Cleaned up code

--- rod.c
+++ rod.c
@@ -1,54 +1,72 @@
 #include <stdio.h>  
 #include "rngs.h"
 
-int main(void){
-    double i;
-    long int x;
-    int win = 0, loss = 0;
-    int attempts = 10000000;
+int main( void ){
+    // The amount of wins (triangles) and losses (not triangles)
+    int win = 0, loss = 0; 
 
-    SelectStream(1);
-    PutSeed(453248854);
-    SelectStream(0);
+    // The amount of times we want to run the simulation
+    int runs = 10000000;
 
-    // for (int l = 0; l  < 10000001; l++)
-        // i = Random();
-    // GetSeed(&x);
-    // printf("Seed is: %d",x);
-    // The seeds are 10000001 apart, 453248854
+    // Default value, placed in stream 0
+    // SelectStream( 0 );
+    // PutSeed( 123456789 );
 
+    // Seed 10,000,001 cycles away, placed in stream 1
+    SelectStream( 1 );
+    PutSeed( 453248854 );
+
+
+    /* ----------------------------------------------------------
+     * The calculation to get the seed 10,000,001 cycles away    |
+     * double i, long int x;                                     |
+     * for ( int l = 0; l < 10000001; l++ )                      |
+     *     i = Random();                                         |
+     * GetSeed( &x );                                            |
+     * printf( "Seed is: %d", x );                               |
+     * ----------------------------------------------------------                      
+     */
+
+    // Declaration of points
     double p1, p2, temp;
-    for(int l = 0; l < attempts; l++){
-        SelectStream(0);
+
+    // The for loop to run our simulation
+    for( int cycles = 0; cycles < runs; cycles++ ){
+        
+        // Go to stream 0 and get a random value, iterates stream 0
+        SelectStream( 0 );
         p1 = Random();
         
-        SelectStream(1);
+        // Go to stream 1 and get a random value, iterates stream 1
+        SelectStream( 1 );
         p2 = Random();
 
-        if (p2 < p1){
+        // The switch statement if p1 is less than p2
+        if ( p2 < p1 ){
             temp = p2;
             p2 = p1;
             p1 = temp;
         }
 
-        double break1, break2, break3;
-        break1 = p1;
-        break2 = p2 - p1;
-        break3 = 1 - p2;
+        // The sides that are created
+        double side1, side2, side3;
+        side1 = p1;
+        side2 = p2 - p1;
+        side3 = 1 - p2;
 
-        if ( (.5 < break1) || (.5 < break2) || (.5 < break3) ){
+        // If any of them are over .5, then it's not a triangle
+        if ( ( .5 < side1 ) || ( .5 < side2 ) || ( .5 < side3 ) )
             loss++;
-        }
-        else{    
-            win++;}
+        else
+            win++;
     }
 
-    printf("wins: %d, loss: %d\n",win,loss);
-    double winrate = win / ((double)attempts);
-    double lossrate = loss / ((double)attempts);
-    printf("winrate: %f, lossrate: %f",winrate,lossrate);
+    // Calculate the rate it's a triangle, and the rate it's not
+    double winrate  = win  / ( ( double ) runs );
+    double lossrate = loss / ( ( double ) runs );
 
-
+    printf( "wins: %d, loss: %d\n", win, loss );
+    printf( "winrate: %f, lossrate: %f", winrate, lossrate );
 
     return 0;
 }
